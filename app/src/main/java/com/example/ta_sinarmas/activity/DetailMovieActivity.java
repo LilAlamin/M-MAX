cb097ce9a4d2af15e473ca1a92c4f7c9ae4fbf5d
LilAlamin <ronaldramadhani14@gmail.com>
2023-05-10 23:36:07 +0700

    Sudah Bisa Detail Tapi rating belum keluar

--- app/src/main/java/com/example/ta_sinarmas/activity/DetailMovieActivity.java
+++ app/src/main/java/com/example/ta_sinarmas/activity/DetailMovieActivity.java
@@ -2,15 +2,47 @@ package com.example.ta_sinarmas.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.ta_sinarmas.R;
 
 public class DetailMovieActivity extends AppCompatActivity {
-
+    TextView judul,deskripsi,rate;
+    ImageView poster;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_movie);
+
+        judul = findViewById(R.id.movie_title);
+        deskripsi = findViewById(R.id.movie_overview);
+        poster = findViewById(R.id.movie_poster);
+        rate = findViewById(R.id.movie_rating);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+        if (bundle != null) {
+            String judulFilm = bundle.getString("movieTitle");
+            String overview_mov = bundle.getString("movieOverview");
+            String image_path = bundle.getString("movieImagePath");
+            String rating = bundle.getString("Rating");
+
+            judul.setText(judulFilm);
+            deskripsi.setText(overview_mov);
+            rate.setText(rating);
+
+            Glide.with(this)
+                    .load("https://image.tmdb.org/t/p/w500" + image_path)
+                    .into(poster);
+        }
+        
+
+
+
     }
 }
--- app/src/main/java/com/example/ta_sinarmas/adapter/MovieAdapter.java
+++ app/src/main/java/com/example/ta_sinarmas/adapter/MovieAdapter.java
@@ -1,7 +1,9 @@
 package com.example.ta_sinarmas.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -13,6 +15,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.example.ta_sinarmas.ItemMovie;
 import com.example.ta_sinarmas.R;
+import com.example.ta_sinarmas.activity.DetailMovieActivity;
 
 import java.util.List;
 
@@ -39,6 +42,24 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MyViewHolder
         holder.tvTitle.setText(resultList.get(position).getTitle());
         holder.tvDescription.setText(resultList.get(position).getOverview());
         Glide.with(context).load("https://image.tmdb.org/t/p/w500" + resultList.get(position).getPosterPath()).into(holder.imgposter);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the selected movie
+                ItemMovie selectedItem = resultList.get(holder.getAdapterPosition());
+
+                // Create Intent to open detail page
+                Intent intent = new Intent(context, DetailMovieActivity.class);
+                // Add selected movie data to Intent
+                intent.putExtra("movieTitle", selectedItem.title);
+                intent.putExtra("movieImagePath", selectedItem.posterPath);
+                intent.putExtra("movieOverview", selectedItem.overview);
+                intent.putExtra("Rating",selectedItem.rateMovie);
+                // Start detail activity
+                context.startActivity(intent);
+            }
+        });
+
     }
 
     @Override
