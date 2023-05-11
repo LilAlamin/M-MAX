048c406c72e48660d4f530cd5c6b15c449e99e9e
LilAlamin <ronaldramadhani14@gmail.com>
2023-05-11 21:07:02 +0700

    4Update 1.1

--- app/src/main/java/com/example/ta_sinarmas/activity/DetailMovieActivity.java
+++ app/src/main/java/com/example/ta_sinarmas/activity/DetailMovieActivity.java
@@ -1,9 +1,11 @@
 package com.example.ta_sinarmas.activity;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,8 +13,9 @@ import com.bumptech.glide.Glide;
 import com.example.ta_sinarmas.R;
 
 public class DetailMovieActivity extends AppCompatActivity {
-    TextView judul,deskripsi,rate;
+    TextView judul, deskripsi, rate;
     ImageView poster;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,24 +25,41 @@ public class DetailMovieActivity extends AppCompatActivity {
         deskripsi = findViewById(R.id.movie_overview);
         poster = findViewById(R.id.movie_poster);
 
-        Intent intent = getIntent();
-        Bundle bundle = intent.getExtras();
+        // Menampilkan tombol back pada action bar
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
 
-        if (bundle != null) {
-            String judulFilm = bundle.getString("movieTitle");
-            String overview_mov = bundle.getString("movieOverview");
-            String image_path = bundle.getString("movieImagePath");
+            Intent intent = getIntent();
+            Bundle bundle = intent.getExtras();
 
-            judul.setText(judulFilm);
-            deskripsi.setText(overview_mov);
+            if (bundle != null) {
+                String judulFilm = bundle.getString("movieTitle");
+                String overview_mov = bundle.getString("movieOverview");
+                String image_path = bundle.getString("movieImagePath");
 
-            Glide.with(this)
-                    .load("https://image.tmdb.org/t/p/w500"+ image_path)
-                    .into(poster);
+                judul.setText(judulFilm);
+                deskripsi.setText(overview_mov);
+
+                Glide.with(this)
+                        .load("https://image.tmdb.org/t/p/w500" + image_path)
+                        .into(poster);
+            }
+
+
+        }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            // Tombol back di action bar ditekan
+            onBackPressed();
+            return true;
         }
 
-
-
-
+        return super.onOptionsItemSelected(item);
     }
+
 }
